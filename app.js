8a41b3341bcffb4e8b795150b8d27ee516117336
CrisRod <crodriguez85@gmail.com>
2019-09-05 09:44:38 -0400

    Finish basi app with typeDefs and resolvers

--- app.js
+++ app.js
@@ -1,9 +1,26 @@
 const express = require('express');
 const app= express();
-const { ApolloServer } = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
 
-const typeDefs = null;
-const resolvers = null;
+const typeDefs = gql`
+    type Query {
+        me: User
+    }
+
+    type User {
+        name: String!
+    }
+`;
+
+const resolvers = gql`
+    Query: {
+        me: () => {
+            return {
+                name: 'Cristian'
+            }
+        }
+    }
+`;
 
 const server = new ApolloServer({
     typeDefs,
