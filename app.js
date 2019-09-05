94ecd308d2d503c1375105458e2af9dc10ecb9a3
CrisRod <crodriguez85@gmail.com>
2019-09-05 09:39:25 -0400

    basic app.js

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app= express();
+const { ApolloServer } = require('apollo-server-express');
+
+const typeDefs = null;
+const resolvers = null;
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+});
+
+server.applyMiddleware({ app });
+
+app.listen(3000, () => console.info('Apollo GraphQL server on port 3000') );
