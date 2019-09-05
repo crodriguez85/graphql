4fc6d0b7d2712e6e62a13ba531ecbaf24b5f9a8d
CrisRod <crodriguez85@gmail.com>
2019-09-05 09:54:09 -0400

    Graphql static data

--- app.js
+++ app.js
@@ -2,24 +2,27 @@ const express = require('express');
 const app= express();
 const { ApolloServer, gql } = require('apollo-server-express');
 const port = 3000
+const users = require('./data').users;
+
+const me = users[0];
 
 const typeDefs = gql`
     type Query {
+        users: [User]
         me: User
     }
 
     type User {
+        id: ID!
         name: String!
+
     }
 `;
 
 const resolvers = {
     Query: {
-        me: () => {
-            return {
-                name: 'Cristian'
-            }
-        }
+        users: () => users,
+        me: () => me
     }
 };
 
