5cda51270ca68013147e423e4011c023a602ef0d
CrisRod <crodriguez85@gmail.com>
2019-09-06 15:15:37 -0400

    db config

--- models/database.js
+++ models/database.js
@@ -1,5 +1,14 @@
 const Sequelize = require('sequelize');
+require('dotenv').config();
 // ('dbName', 'userName', 'pass')
-const sequelize = new Sequelize('graphql', 'root', 'cra260385', {
-    
+const sequelize = new Sequelize('graphql', process.env.DB_USER, process.env.DB_PASS, {
+    dialect: 'mysql',
+    operatorsAliases: false,
+    define: {
+        timestamps: false
+    }
 });
+
+module.exports = {
+    sequelize
+};
