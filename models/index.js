7859070b15db0288c34e0b078edd6f091115c926
CrisRod <crodriguez85@gmail.com>
2019-09-06 15:39:05 -0400

    index db

--- models/index.js
+++ models/index.js
@@ -1,4 +1,17 @@
 const { sequelize } = require('./database');
 
-const UserModel =
-const CarModel = 
+const UserModel = sequelize.import('./user.js');
+const CarModel = sequelize.import('./car.js');
+
+const models = {
+    User: UserModel,
+    Car: CarModel
+}
+
+Object.keys(models).forEach(key => {
+    if('associate' in models[key]) {
+        models[key].associate(models);
+    }
+});
+
+module.exports = models;
