fb29d7b2965d10eedc5a8fbb301f628c293d180e
CrisRod <crodriguez85@gmail.com>
2019-09-06 14:40:58 -0400

    extracting models as the context

--- resolvers/user.js
+++ resolvers/user.js
@@ -1,6 +1,6 @@
 const resolvers = {
     Query: {
-        users: (root, args, context) => users,
+        users: (root, args, { models }) => users,
         // Query with Parameters id
         user:(root, { id }) => {
             const user = users.filter(user => user.id === id);
