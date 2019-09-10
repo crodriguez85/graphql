469bc375c220d01b124fb2dd47494e3cd05df3cc
CrisRod <crodriguez85@gmail.com>
2019-09-10 16:44:04 -0300

    user

--- client/src/App.js
+++ client/src/App.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ApolloClient from 'apollo-boost';
 import { ApolloProvider } from 'react-apollo';
 
+import User from './components/User';
+
 client = new ApolloClient({
   uri: 'http://localhost:3000/graphql'
 });
 
 function App() {
   return (
-    <div className="App">
-      Hola
+    <div>
+      <ApolloProvider client={client}>
+        <User/>
+      </ApolloProvider>
     </div>
   );
 }
---
+++ client/src/components/User.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Query } from 'react-apollo';
+
+const User = () => {
+    return ( 
+        null
+     );
+}
+ 
+export default User;
