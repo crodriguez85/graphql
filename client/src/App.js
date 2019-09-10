a81697139de92b0402eb78f1bc2b2c0da586faaf
CrisRod <crodriguez85@gmail.com>
2019-09-10 16:40:49 -0300

    apolloclient

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql'
+});
 
 function App() {
   return (
