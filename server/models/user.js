525c801d3ac60a18264c7b69c3c7b1ae4c8c0ce5
CrisRod <crodriguez85@gmail.com>
2019-09-10 18:07:29 -0300

    model add user y pass

--- server/models/user.js
+++ server/models/user.js
@@ -2,6 +2,19 @@ const user = (sequelize, DataTypes) => {
     const User = sequelize.define('user', {
         name: {
             type: DataTypes.STRING,
+        },
+        username: {
+            type: DataTypes.STRING,
+            unique: true,
+            validate: {
+                notEmpty: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            validate: {
+                notEmpty: true,
+            }
         }
     });
 
