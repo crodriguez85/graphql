46a7dc93ea2050ff4a94c90d8ee34c7be38d7480
CrisRod <crodriguez85@gmail.com>
2019-09-10 16:09:28 -0300

    cors

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const app= express();
 const { ApolloServer } = require('apollo-server-express');
 const port = 3000
+const cors = require('cors');
 
 const models = require('./models');
 const typeDefs = require('./typeDefs');
@@ -21,5 +22,6 @@ const server = new ApolloServer({
 });
 
 server.applyMiddleware({ app });
+app.use(cors());
 
 app.listen(port, () => console.info(`Apollo GraphQL server on  localhost:${3000}/graphql`) );
