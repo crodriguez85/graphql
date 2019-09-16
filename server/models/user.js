d514e3a3317f2f77327d7420d831250af207e98a
CrisRod <crodriguez85@gmail.com>
2019-09-16 16:41:15 -0300

    bcrypt compare user model

--- server/models/user.js
+++ server/models/user.js
@@ -24,6 +24,10 @@ const user = (sequelize, DataTypes) => {
         return await bcrypt.hash(this.password, 10);
     }
 
+    User.prototype.validatePassword = async function(password) {
+        return await bcrypt.compare(password, this.password);
+    }
+
     User.associate = models => {
         //  One to many one user many cars
         User.hasMany(models.Car, { onDelete: 'CASCADE'})
